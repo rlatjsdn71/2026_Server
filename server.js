66191000c6cd7d944fb70e705e457d2e98fb962b
김선우 <rlatjsdn0701@gmail.com>
2026-04-22 17:35:26 +0900

    study: connect to DB, insert, find

--- server.js
+++ server.js
@@ -5,10 +5,25 @@ const app = express()
 app.use(express.static(__dirname + '/public'));
 // 경로를 지정해야 css파일, 이미지 파일같은 보조 파일(static파일) 불러올 수 있음
 
-// 8080은 포트번호
-app.listen(8080, () => {
-    console.log('http://localhost:8080 에서 서버 실행중')
-})
+// 이하 mongodb 라이브러리 설정
+const { MongoClient } = require('mongodb') // mongodb 라이브러리 불러오기
+
+let db;
+ // 여기에 db 접속 url과 username, password 입력
+const url = `mongodb://admin:dYvOhN6kQlhvQmzv@ac-smg1vck-shard-00-00.meysf13.mongodb.net:27017,
+ac-smg1vck-shard-00-01.meysf13.mongodb.net:27017,ac-smg1vck-shard-00-02.meysf13.mongodb.net:27017
+/?ssl=true&replicaSet=atlas-rqe29b-shard-0&authSource=admin&appName=Cluster0`;
+new MongoClient(url).connect().then((client) => { // mongdodb와 연결
+    console.log('DB연결성공')
+    db = client.db('forum') // forum 데이터 베이스와 연결
+    // 8080은 포트번호
+    app.listen(8080, () => {
+        console.log('http://localhost:8080 에서 서버 실행중')
+    })
+}).catch((err) => {
+    console.log(err)
+});
+
 
 // 메인페이지 접속 시(get 요청 시) 콜백 함수 실행(/ = 메인페이지)
 app.get('/', (요청, 응답) => {
@@ -18,7 +33,9 @@ app.get('/', (요청, 응답) => {
 
 // 라우팅 (/news 페이지)
 app.get('/news', (요청, 응답) => {
-    응답.send('뉴스다옹~')
+    db.collection('post').insertOne({title:'메롱메롱 인서트'}); // post 컬렉션에 내용 입력
+    // 컬렉션은 대충 폴더라고 생각
+    // 응답.send('뉴스다옹~')
 })
 
 app.get('/shop', (요청, 응답) => {
@@ -26,5 +43,14 @@ app.get('/shop', (요청, 응답) => {
 })
 
 app.get('/about', (요청, 응답) => {
-    응답.sendFile(__dirname + '/about.html');
+    // db에 데이터 입력
+    // 응답.sendFile(__dirname + '/about.html');
+})
+
+app.get('/list', async (요청, 응답) => {
+    let res = await db.collection('post').find().toArray(); // post 컬렉션에 있는 모든 내용 불러오기
+    // await = 비동기 실행을 동기 처리
+
+    console.log(res[0].title); // 서버에서 console.log 하면 터미널에 출력됨
+    응답.send('halu')
 })
